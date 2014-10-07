7c39644706e62c7d53765c92bfcfddd5c541c862
jayxigua <jianbin.zou>
2014-10-07 23:44:02 +0800

    exploitsAddition Transfer to ExploitsStateValue

--- LotteryRiskCalculation/src/main/java/jayxigua/LotteryRiskCalculation/soccer/service/CalculateService.java
+++ LotteryRiskCalculation/src/main/java/jayxigua/LotteryRiskCalculation/soccer/service/CalculateService.java
@@ -1,10 +1,8 @@
 package jayxigua.LotteryRiskCalculation.soccer.service;
 
-import jayxigua.LotteryRiskCalculation.soccer.entity.ExploitsElement;
 import jayxigua.LotteryRiskCalculation.soccer.entity.ExploitsStateValue;
 import jayxigua.LotteryRiskCalculation.soccer.entity.SoccerMatch;
 import jayxigua.LotteryRiskCalculation.soccer.entity.SoccerTeam;
-import jayxigua.LotteryRiskCalculation.soccer.util.MyNumberUtils;
 
 public class CalculateService {
 
@@ -29,24 +27,8 @@ public class CalculateService {
 	 * @return
 	 */
 	static ExploitsStateValue calculateTeamStrength(SoccerTeam team) {
-		ExploitsStateValue ef = new ExploitsStateValue();
-		ef.setWin(MyNumberUtils.getBaseIncrease("", team.getLast10Exploits()));
-		return ef;
+		ExploitsStateValue esv = new ExploitsStateValue();
+		return esv.exploitsAddition(team.getLast10Exploits(), "10").exploitsAddition(team.getLast10homeVisitingExploits(), "10");
 	}
 
-	/**
-	 * 因素叠加
-	 * 
-	 * @param original
-	 * @param exploits
-	 * @param base
-	 * @return
-	 */
-	static ExploitsStateValue exploitsAddition(ExploitsStateValue original, ExploitsElement exploits, String base) {
-		ExploitsStateValue target = new ExploitsStateValue();
-		target.setWin(original.getWin().multiply(MyNumberUtils.getBaseIncrease(base, exploits.getWin().toString())));
-		target.setWin(original.getPlanish().multiply(MyNumberUtils.getBaseIncrease(base, exploits.getPlanish().toString())));
-		target.setWin(original.getLose().multiply(MyNumberUtils.getBaseIncrease(base, exploits.getLose().toString())));
-		return target;
-	}
 }
