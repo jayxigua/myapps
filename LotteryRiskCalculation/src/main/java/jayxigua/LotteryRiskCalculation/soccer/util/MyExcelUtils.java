47333ac05da8bd1348423b0537c118f646c16cc6
jayxigua <jianbin.zou>
2014-10-07 19:01:49 +0800

    loadExcelByXslx

--- LotteryRiskCalculation/src/main/java/jayxigua/LotteryRiskCalculation/soccer/util/MyExcelUtils.java
+++ LotteryRiskCalculation/src/main/java/jayxigua/LotteryRiskCalculation/soccer/util/MyExcelUtils.java
@@ -1,35 +1,25 @@
 package jayxigua.LotteryRiskCalculation.soccer.util;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class MyExcelUtils {
 
-	public static void loadExcelByXslx() {
+	public static XSSFSheet loadExcelByXslx(String sheetName) {
 		try {
-			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(
-					"workbooks.xlsx"));
-			XSSFSheet sheet = workbook.getSheet("new sheet");
-			XSSFRow row1 = sheet.getRow(0);
-			if (row1 != null) {
-				System.out.println(row1.getCell(0).getStringCellValue());
-			}
-			XSSFRow row2 = sheet.getRow(1);
-			if (row2 == null) {
-				System.out.println("Null");
-			}
-
+			InputStream in = MyExcelUtils.class
+					.getResourceAsStream("/500/2014-10-07_足球彩票_风险收益（赔率加赛况）计算.xlsx");
+			XSSFWorkbook workbook = new XSSFWorkbook(in);
+			return workbook.getSheet(sheetName);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
 	}
 }
---
+++ LotteryRiskCalculation/src/test/java/jayxigua/LotteryRiskCalculation/soccer/util/test/MyExcelUtilsTest.java
@@ -0,0 +1,14 @@
+package jayxigua.LotteryRiskCalculation.soccer.util.test;
+
+import jayxigua.LotteryRiskCalculation.soccer.util.MyExcelUtils;
+import jayxigua.LotteryRiskCalculation.soccer.util.MyNumberUtils;
+
+import org.junit.Test;
+
+public class MyExcelUtilsTest {
+
+	@Test
+	public void loadExcelByXslx() {
+		MyExcelUtils.loadExcelByXslx();
+	}
+}
